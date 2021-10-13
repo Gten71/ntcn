7fddbbc75eac22dd09fcd9c59ff46f8b42eade35
Maxim <Maxim@DESKTOP-ICMSR5Q>
2021-10-13 14:50:50 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace ntcn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.White;
+
+            int res = 0;
+
+            for (int e= 1000; e >0; e -=7)
+
+            {
+                res = e - 7;
+                Console.WriteLine(e + "-" + 7 + "=" + res);
+                Thread.Sleep(100);
+
+
+            }
+
+
+        }
+
+      
+
+
+
+
+
+    }
+}
